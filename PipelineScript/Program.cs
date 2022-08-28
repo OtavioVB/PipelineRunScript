1da16c0b805099f642d2e2f74f6834d391087da4
OtavioVB <otaviovb.developer@gmail.com>
2022-08-28 13:10:16 -0300

    base(archives): `Initial Commit`

---
+++ PipelineScript/Program.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Pipeline;
+
+public class Program
+{
+    static void Main()
+    {
+        var dataConnection = new MySqlConnection("server=127.0.0.1;database=pipeline;uid=otavio;pwd=1234;port=3306");
+        dataConnection.Open();
+        var command = dataConnection.CreateCommand();
+        command.CommandText = "INSERT INTO TB_EXAMPLE (N_ID, T_NOME) VALUES (@ID, @Nome)";
+        command.Parameters.AddWithValue("@ID", 1);
+        command.Parameters.AddWithValue("@Nome", "Otavio");
+        command.ExecuteNonQuery();
+    }
+}
